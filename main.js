40f13c7e5da878f7beecf7ffce0e815f2d0df2f1
hoon_ground <break_the_limit@naver.com>
2025-01-02 17:43:13 +0900

    메인페이지 제작

---
+++ main.js
@@ -0,0 +1,34 @@
+let target = document.querySelector("#dynamic");
+
+//커서 깜빡임 효과
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink, 500);
+
+function randomString(){
+    let stringArr = ["Do! and then regret.", "Positive power can change my life.", "Better tomorrow than yesterday."];
+    let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+    let selectStringArr = selectString.split("");
+
+    return selectStringArr;
+}
+
+function resetTyping(){
+    target.textContent = "";
+    dynamic(randomString());
+}
+
+//텍스트 출력함수
+function dynamic(randomArr){
+    if(randomArr.length > 0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        }, 80);
+    }else{
+        setTimeout(resetTyping, 1000);
+    }
+}
+
+dynamic(randomString());
